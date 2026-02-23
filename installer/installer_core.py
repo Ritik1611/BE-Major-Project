01f9c873fd416356146404622d4f55912d836b54
Ritik1611 <2022.ritik.shetty@ves.ac.in>
2026-02-23 09:48:46 +0530

    Installer updated

--- installer/installer_core.py
+++ installer/installer_core.py
@@ -45,7 +45,7 @@ BASE_DIR = Path.home() / ".federated"
 STATE_FILE = BASE_DIR / "state" / "install_state.json"
 KEYS_DIR = BASE_DIR / "keys"
 
-SERVER_ADDR = "tcp://0.tcp.in.ngrok.io:18767"
+SERVER_ADDR = "tcp://0.tcp.in.ngrok.io:19226"
 INSTALLER_OTP = None
 
 
